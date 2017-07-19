9abda63b27cd0eb1766d74291e62562cc3b31eb2
huge <2252603873@qq.com>
2017-07-19 08:13:17 +0800

    脚本描述变更

--- bin/start.sh
+++ bin/start.sh
@@ -5,11 +5,11 @@ if [ $? -ne 0 ]
 then
   sudo /Users/xuefeihu/software/openresty/nginx/sbin/nginx -t -q -c /Users/xuefeihu/hugege/code-sublime/moguhu/config/nginx.conf 
   sudo /Users/xuefeihu/software/openresty/nginx/sbin/nginx -c /Users/xuefeihu/hugege/code-sublime/moguhu/config/nginx.conf -p /Users/xuefeihu/hugege/code-sublime/moguhu/
-  echo "nginx start"
+  echo "openresty start"
 else
   sudo /Users/xuefeihu/software/openresty/nginx/sbin/nginx -t -q -c /Users/xuefeihu/hugege/code-sublime/moguhu/config/nginx.conf
   sudo /Users/xuefeihu/software/openresty/nginx/sbin/nginx -c /Users/xuefeihu/hugege/code-sublime/moguhu/config/nginx.conf -p /Users/xuefeihu/hugege/code-sublime/moguhu/ -s reload
-  echo "nginx reload"
+  echo "openresty reload"
 fi
 echo -e "#####################################################\n\n"
 tail -f /Users/xuefeihu/hugege/code-sublime/moguhu/logs/error.log
--- bin/stop.sh
+++ bin/stop.sh
@@ -3,6 +3,6 @@
 sudo /Users/xuefeihu/software/openresty/nginx/sbin/nginx -t -q -c /Users/xuefeihu/hugege/code-sublime/moguhu/config/nginx.conf
 sudo /Users/xuefeihu/software/openresty/nginx/sbin/nginx -c /Users/xuefeihu/hugege/code-sublime/moguhu/config/nginx.conf -p /Users/xuefeihu/hugege/code-sublime/moguhu/ -s stop
 
-echo "nginx stop"
+echo "openresty stop"
 echo -e "#####################################################\n\n"
 tail -f /Users/xuefeihu/hugege/code-sublime/moguhu/logs/error.log
